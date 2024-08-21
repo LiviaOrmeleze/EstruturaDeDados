b6d05b3a65861220ecd64eb489e4772168097dfe
Livia M. Ormeleze <lormeleze@gmail.com>
2024-08-21 10:38:40 -0300

    Trabalhando com Branchs, teste 2.

--- EstruturaDeDados/EstruturaDeDados/Program.cs
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("My first branch!");
+        Console.WriteLine("Trabalhando com Git\\Github");
+        Console.WriteLine("Jesus te ama");
     }
 }
